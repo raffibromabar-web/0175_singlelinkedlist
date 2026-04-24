5a02fdbbf0f9b7da5304a655fa24834b8cb3de62
raffibromabar-web <raffibromabar@gmail.com>
2026-04-24 15:24:27 +0700

    first commit

---
+++ singly_likedlist.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+    public:
+        int noMhs;
+        Node *next;
+};
+
+class LinkedList
+{
+    Node *START;
+
+public:
+    LinkedList()
+    {
+        START
+    }
+};
